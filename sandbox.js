785a60aa009537de0689178470e02b225fbbf748
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-13 21:27:04 +0200

    v.3.1

--- sandbox.js
+++ sandbox.js
@@ -5,17 +5,17 @@ var distance = prompt('Please insert the distance of your journey');
 console.log(customerName + ' ' + customerAge + ' ' + distance);
 
 // ticket price variables
-var price = distance * 0.21;
+var basicPrice = distance * 0.21;
 var journeyPrice;
-console.log(price);
+console.log(basicPrice);
 
 // ticket price
 if (customerAge < 18) {
-    journeyPrice = price * 0.8;
+    journeyPrice = basicPrice * 0.8;
 } else if (customerAge > 65) {
-    journeyPrice = price * 0.6;
+    journeyPrice = basicPrice * 0.6;
 } else {
-    journeyPrice = price;
+    journeyPrice = basicPrice;
 }
 console.log(journeyPrice);
 
@@ -25,7 +25,7 @@ console.log(journeyPriceFixed);
 
 // output
 if (isNaN(customerAge) || isNaN(distance)) {
-    document.getElementsByClassName('output')[0].style.display = 'none';
+    document.getElementById('output').style.display = 'none';
     document.getElementById('warnings').innerHTML = 'Invalid entries. Please refresh the page.';
 } else {
     document.getElementById('ticket-price').innerHTML = journeyPriceFixed;
