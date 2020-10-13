c26ce254e690d66681501e0a6fc04c8805bfb595
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-13 16:40:41 +0200

    end v.02

--- sandbox.js
+++ sandbox.js
@@ -2,11 +2,12 @@
 var customerName = prompt('Please insert your name');
 var customerAge = prompt('Please insert your age');
 var distance = prompt('Please insert the distance of your journey');
+console.log(customerName + ' ' + customerAge + ' ' + distance);
 
 // ticket price variables
 var price = distance * 0.21;
 var journeyPrice;
-// console.log(price);
+console.log(price);
 
 // ticket price
 if (customerAge < 18) {
@@ -16,8 +17,16 @@ if (customerAge < 18) {
 } else {
     journeyPrice = price;
 }
-// console.log(journeyPrice);
+console.log(journeyPrice);
+
+// journey price fixed
+var journeyPriceFixed = journeyPrice.toFixed(2);
+console.log(journeyPriceFixed);
 
 // output
-document.getElementById('customer-name').innerHTML = customerName;
-document.getElementById('ticket-price').innerHTML = journeyPrice;
+if (isNaN(customerAge) || isNaN(distance)) {
+    document.getElementById('warnings').innerHTML = 'Invalid entries. Please refresh the page.';
+} else {
+    document.getElementById('customer-name').innerHTML = 'Hi ' + customerName;
+    document.getElementById('ticket-price').innerHTML = 'the price of your journey is: ' +  journeyPriceFixed + '€';
+}
